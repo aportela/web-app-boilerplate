def5499f83965c3a78c758ec8cff37da822b8b91
alex <766f6964+github@gmail.com>
2017-11-19 18:48:44 +0100

    updated middleware to add log support

--- src/Middleware/APIExceptionCatcher.php
+++ src/Middleware/APIExceptionCatcher.php
@@ -5,6 +5,13 @@
     namespace Foobar\Middleware;
 
     class APIExceptionCatcher {
+
+        private $container;
+
+        public function __construct($container) {
+            $this->container = $container;
+        }
+
         /**
          * Example middleware invokable class
          *
@@ -17,15 +24,21 @@
         public function __invoke($request, $response, $next)
         {
             try {
+                $this->container["apiLogger"]->info($request->getOriginalMethod() . " " . $request->getUri()->getPath());
+                $this->container["apiLogger"]->debug($request->getBody());
                 $response = $next($request, $response);
                 return $response;
             } catch (\Foobar\Exception\InvalidParamsException $e) {
+                $this->container["apiLogger"]->debug("Exception caught: " . $e->getMessage());
                 return $response->withJson(['invalidOrMissingParams' => array($e->getMessage())], 400);
             } catch (\Foobar\Exception\NotFoundException $e) {
+                $this->container["apiLogger"]->debug("Exception caught: " . $e->getMessage());
                 return $response->withJson(['keyNotFound' => $e->getMessage()], 404);
             } catch (\Foobar\Exception\AccessDenied $e) {
+                $this->container["apiLogger"]->debug("Exception caught: " . $e->getMessage());
                 return $response->withJson([], 403);
             } catch (\Throwable $e) {
+                $this->container["apiLogger"]->error("Exception caught: " . $e->getMessage());
                 return $response->withJson(['exceptionDetails' => $e->getMessage()], 500);
             }
         }
