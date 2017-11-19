7bc4706da9ae153a5fbae39abbcf411eaa6c53ec
alex <766f6964+github@gmail.com>
2017-11-19 19:37:51 +0100

    added install/upgrade database commnadline tool

---
+++ tools/install-upgrade-db.php
@@ -0,0 +1,37 @@
+<?php
+
+    declare(strict_types=1);
+
+    require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
+
+    echo "Foobar installer" . PHP_EOL;
+
+    $app = (new \Foobar\App())->get();
+
+    $actualVersion = 0;
+    $v = new \Foobar\Database\Version(new \Foobar\Database\DB());
+    try {
+        $actualVersion = $v->get();
+    } catch (\Foobar\Exception\NotFoundException $e) {
+    } catch (\PDOException $e) {
+    }
+    if ($actualVersion == 0) {
+        echo "Creating database...";
+        $v->install();
+        echo "ok!" , PHP_EOL;
+        $actualVersion = 1.00;
+    }
+    echo sprintf("Upgrading database from version %.2f%s", $actualVersion, PHP_EOL);
+    $result = $v->upgrade();
+    if (count($result["successVersions"]) > 0 || count($result["failedVersions"]) > 0) {
+        foreach($result["successVersions"] as $v) {
+            echo sprintf(" upgrading version to: %.2f: ok!%s", $v, PHP_EOL);
+        }
+        foreach($result["failedVersions"] as $v) {
+            echo sprintf(" upgrading version to: %.2f: error!%s", $v, PHP_EOL);
+        }
+    } else {
+        echo "No database upgrade required" . PHP_EOL;
+    }
+
+?>
