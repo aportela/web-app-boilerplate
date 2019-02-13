b66195361ad84963e6b99f800a463c60ea5929a0
alex <766f6964+github@gmail.com>
2019-02-13 15:29:27 +0100

    set vuejs template as constant (do not return new instance)

--- public/scripts/modules/foobar.js
+++ public/scripts/modules/foobar.js
@@ -1,29 +1,27 @@
 import { mixinErrors as mixinErrors } from './mixins.js';
 import { default as foobarAPI } from './api.js';
 
-const template = function () {
-    return `
-        <section class="hero is-fullheight is-light is-bold" v-if="! loading">
-            <div class="hero-body">
-                <div class="container">
-                    <div class="columns is-vcentered">
-                        <div class="column is-4 is-offset-4">
-                            <h1 class="title has-text-centered">Foobar</h1>
-                            <h2 class="subtitle is-6 has-text-centered">success!</h2>
-                            <p class="has-text-centered">
-                                <a href="https://github.com/aportela/web-app-boilerplate" target="_blank"><span class="icon is-small"><i class="fab fa-github"></i></span>Project page</a> | <a href="mailto:766f6964+github@gmail.com">by alex</a>
-                            </p>
-                        </div>
+const template = `
+    <section class="hero is-fullheight is-light is-bold" v-if="! loading">
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns is-vcentered">
+                    <div class="column is-4 is-offset-4">
+                        <h1 class="title has-text-centered">Foobar</h1>
+                        <h2 class="subtitle is-6 has-text-centered">success!</h2>
+                        <p class="has-text-centered">
+                            <a href="https://github.com/aportela/web-app-boilerplate" target="_blank"><span class="icon is-small"><i class="fab fa-github"></i></span>Project page</a> | <a href="mailto:766f6964+github@gmail.com">by alex</a>
+                        </p>
                     </div>
                 </div>
             </div>
-        </section>
-    `;
-};
+        </div>
+    </section>
+`;
 
 export default {
     name: 'foobar-component',
-    template: template(),
+    template: template,
     data: function () {
         return ({
             loading: false
